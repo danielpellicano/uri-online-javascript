1cc2501c98ef9822017e6ff1a1b746029f648aee
Daniel Curado Pellicano <daniel.pellicano@pampa.compasso>
2020-10-29 10:47:18 -0300

    1001

---
+++ 1001 - Extremamente Básico/script.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+var X = A + B;
+console.log('X = ' + X);
